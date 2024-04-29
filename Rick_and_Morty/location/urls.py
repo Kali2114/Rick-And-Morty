69d6639b89f401a3922755198f7288314b1c3208
Kamil Kalicki <wspolny.motyw@gmail.com>
2024-04-29 19:21:18 +0200

    added new models and serializers

--- Rick_and_Morty/location/urls.py
+++ Rick_and_Morty/location/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import LocationView
+
 urlpatterns = [
+    path('location_view', LocationView.as_view(), name='location_view')
 ]
--- Rick_and_Morty/location/views.py
+++ Rick_and_Morty/location/views.py
@@ -1,3 +1,17 @@
+from django.views import View
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+class LocationView(View):
+    template_name = 'location_view.html'
+
+    def get(self, request, *args, **kwargs):
+        api_url = 'https://rickandmortyapi.com/api/location'
+        try:
+            response = requests.get(api_url)
+            characters = response.json()['results'] if response.status_code == 200 else []
+        except requests.RequestException:
+            context = {'error_message': 'Failed to load data from the Rick and Morty API'}
+            return render(request, self.template_name, context)
+
+        return render(request, self.template_name, {'characters': characters})
